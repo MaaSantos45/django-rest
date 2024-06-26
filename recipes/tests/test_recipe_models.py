bb67080f42f8a74e3b1b6fb4a2b243034987e158
MaaSantos45 <maasantos776@gmail.com>
2024-06-26 13:27:12 -0300

    add class to test category

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -30,10 +30,6 @@ class RecipeModelTest(RecipeTestBase):
     def test_recipe_model_title_str(self):
         self.assertEqual(str(self.recipe), self.recipe.title)
 
-    def test_category_model_name_str(self):
-        category = self.make_category()
-        self.assertEqual(str(category), category.name)
-
     # using subTest
     # def test_recipe_model_fields_max_length(self):
     #     fields = [
@@ -57,15 +53,6 @@ class RecipeModelTest(RecipeTestBase):
         with self.assertRaises(e.ValidationError):
             self.recipe.full_clean()
 
-    @parameterized.expand([
-        ('name', 60),
-    ])
-    def test_category_model_fields_max_length(self, field, max_length):
-        category = models.Category()
-        setattr(category, field, 'A' * (max_length + 1))
-        with self.assertRaises(e.ValidationError):
-            category.full_clean()
-
     @skip('Need to validate choices')
     def test_recipe_model_fields_choice(self):
         self.recipe.preparation_time_unit = "hours"
@@ -82,3 +69,22 @@ class RecipeModelTest(RecipeTestBase):
         recipe = self.make_recipe_no_defaults()
 
         self.assertFalse(getattr(recipe, field))
+
+
+class CategoryModelTest(RecipeTestBase):
+    def setUp(self):
+        self.category = self.make_category()
+        return super().setUp()
+
+    def test_category_model_name_str(self):
+        category = self.make_category()
+        self.assertEqual(str(category), category.name)
+
+    @parameterized.expand([
+        ('name', 60),
+    ])
+    def test_category_model_fields_max_length(self, field, max_length):
+        category = models.Category()
+        setattr(category, field, 'A' * (max_length + 1))
+        with self.assertRaises(e.ValidationError):
+            category.full_clean()
